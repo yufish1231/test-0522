1d2360672b29b7030da44b30c389834de244ebc5
yufish1231 <yufish1321@gmail.com>
2026-05-22 11:52:51 +0800

    code

--- src/calculate.py
+++ src/calculate.py
@@ -1,4 +1,20 @@
-def add_func(a, b):
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {"status": "online", "message": "這是簡易計算機 API"}
+
+
+@app.get("/add")
+def calculate_add(a: float, b: float):
+    result = add_func(a, b)
+    return {"status": "addition", "a": a, "b": b, "result": result}
+
+
+def add_func(a: float, b: float) -> float:
     return a + b
 
 
