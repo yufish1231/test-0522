e3b4b7d31a85d9c646dedfb376fb63504fad6efc
yufish1231 <yufish1321@gmail.com>
2026-05-22 11:37:11 +0800

    test_sub_func

--- test/test_calculate.py
+++ test/test_calculate.py
@@ -1,6 +1,12 @@
 from calculate import add_func
+from calculate import sub_func
 
 def test_add_func():
     assert add_func(1,2) == 3
     assert add_func(0,0) == 0
     assert add_func(-1,-2) == -3
+
+def test_sub_func():
+    assert sub_func(1,2) == -1
+    assert sub_func(0,0) == 0
+    assert sub_func(-1,-2) == 1
